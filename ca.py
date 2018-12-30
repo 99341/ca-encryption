674c3c25e4deec61b61e018e9f7c791ec0a2de3b
Przemysław Salega <Pshemckoo@gmail.com>
2018-12-30 16:45:54 +0100

    Create ca.py
    Stworzenie działającego automatu komórkowego, wraz z metodami potrzebnymi do jego iteracji

---
+++ ca.py
@@ -0,0 +1,58 @@
+class CellularAutomaton:
+    # generation - poczatkowe ustawienie komorek w automacie
+    # uniform_rule - jednakowa zasada dla wszystkich komorek (podajemy jaka)
+    # non_uniform_rules - podajemy liste zasad dla poszczegolnych komorek
+    def __init__(self, generation, uniform_rule=None, non_uniform_rules=None):
+        self.generations_list = []
+        self.current_generation = generation
+        self.automaton_length = len(generation)
+        if uniform_rule is not None:
+            self.rules_list = []
+            for i in range(self.automaton_length):
+                self.rules_list.append(uniform_rule)
+        elif non_uniform_rules is not None:
+            self.rules_list = non_uniform_rules
+
+    # generowanie stanu komorki o indeksie index w nowej generacji
+    # tego nie uzywamy poza klasa
+    def create_new_number(self, index):
+        rule = bin(self.rules_list[index])
+        rule = str(rule[2:]).zfill(8)
+        rule = list(rule)
+        rule.reverse()
+        if self.current_generation[index - 1] == '0' and self.current_generation[index] == '0' and self.current_generation[(index + 1)%self.automaton_length] == '0':
+            return rule[0]
+        if self.current_generation[index - 1] == '0' and self.current_generation[index] == '0' and self.current_generation[(index + 1)%self.automaton_length] == '1':
+            return rule[1]
+        if self.current_generation[index - 1] == '0' and self.current_generation[index] == '1' and self.current_generation[(index + 1)%self.automaton_length] == '0':
+            return rule[2]
+        if self.current_generation[index - 1] == '0' and self.current_generation[index] == '1' and self.current_generation[(index + 1)%self.automaton_length] == '1':
+            return rule[3]
+        if self.current_generation[index - 1] == '1' and self.current_generation[index] == '0' and self.current_generation[(index + 1)%self.automaton_length] == '0':
+            return rule[4]
+        if self.current_generation[index - 1] == '1' and self.current_generation[index] == '0' and self.current_generation[(index + 1)%self.automaton_length] == '1':
+            return rule[5]
+        if self.current_generation[index - 1] == '1' and self.current_generation[index] == '1' and self.current_generation[(index + 1)%self.automaton_length] == '0':
+            return rule[6]
+        if self.current_generation[index - 1] == '1' and self.current_generation[index] == '1' and self.current_generation[(index + 1)%self.automaton_length] == '1':
+            return rule[7]
+
+    # iteruje automat komorkowy
+    # stara generacja jest zapisywana na liscie
+    # nowa przypisywana jest do pola current_generation
+    def iterate(self):
+        new_gen = []
+        for i in range(self.automaton_length):
+            new_gen.append(self.create_new_number(i))
+        self.generations_list.append(self.current_generation)
+        self.current_generation = new_gen
+
+
+# egzampl kod
+cellular = CellularAutomaton(['1', '0', '1', '1', '1'], non_uniform_rules=[53, 110, 34, 21, 3])
+print("Rules: " + str(cellular.rules_list))
+print(cellular.current_generation)
+for i in range(10):
+    cellular.iterate()
+    print(cellular.current_generation)
+
