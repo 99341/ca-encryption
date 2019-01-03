daadf66b2da81aeb6d190d50d5080a8fef785e66
Mateusz Halada <mateuszowy@hotmail.com>
2019-01-03 15:56:23 +0100

    dodano zapisywanie do pliku

--- encrypting.py
+++ encrypting.py
@@ -8,6 +8,7 @@ class Secret_key(QMainWindow):
         QMainWindow.__init__(self, parent)
         self.ui = encrypt_ui.Ui_Dialog()
         self.cellular = None
+        self.rules = None
         self.ui.setupUi(self)
         self.ui.encrypt_key.clicked.connect(self.encrypt_clicked) #podlacza przycisk do funckji
         self.ui.decrypt_key.clicked.connect(self.decrypt_clicked)
@@ -33,14 +34,39 @@ class Secret_key(QMainWindow):
                 tmp += x2[n]
         return tmp
 
+    def check_if_non_uniform(self):
+        tmp = self.ui.rule_box.text()
+        for n in range(len(tmp)):
+            if tmp[n] == " ":
+                return True
+
+        return False
+
     @pyqtSlot()
     def encrypt_clicked(self): #zmienai na postac binarna jesli klucz nie jest pusty i robi XOR
         if self.ui.rule_box.text() is not "": #wykona sie tylko jezeli podamy regule
-            if self.ui.initial_state.text() is "": #tutaj wchodzi bez podanych wartosci poczatkowych
-                self.cellular = ca.CellularAutomaton(self.losowe_zera_i_jedynki(len(self.ui.text_binary_encrypted.text())+random.randrange(10)), int(self.ui.rule_box.text()))
-            else:  #tutaj wchodzi z podanymi wartosciami poczatkowymi
-                self.cellular = ca.CellularAutomaton(
-                    self.nielosowe_zera_i_jedynki() , int(self.ui.rule_box.text()))
+
+            if self.check_if_non_uniform():
+                tmp = self.ui.rule_box.text()
+                self.rules =  tmp.split(' ')
+                self.rules = list(map(int, self.rules))
+            else:
+                self.rules = [self.ui.rule_box.text()]
+
+            if len(self.rules) == 1:
+                self.rules = int(self.rules[0])
+
+                if self.ui.initial_state.text() is "": #tutaj wchodzi bez podanych wartosci poczatkowych
+                    self.cellular = ca.CellularAutomaton(self.losowe_zera_i_jedynki(len(self.ui.text_binary_encrypted.text())+random.randrange(10)), self.rules)
+                else:  #tutaj wchodzi z podanymi wartosciami poczatkowymi
+                    self.cellular = ca.CellularAutomaton(
+                        self.nielosowe_zera_i_jedynki() , self.rules)
+            else:
+                if self.ui.initial_state.text() is "": #tutaj wchodzi bez podanych wartosci poczatkowych
+                    self.cellular = ca.CellularAutomaton(self.losowe_zera_i_jedynki(len(self.rules)), non_uniform_rules=self.rules)
+                else:  #tutaj wchodzi z podanymi wartosciami poczatkowymi
+                    self.cellular = ca.CellularAutomaton(
+                        self.nielosowe_zera_i_jedynki() , non_uniform_rules=self.rules)
 
             self.iterate_iterate() # tworzy generacje
 
@@ -57,6 +83,25 @@ class Secret_key(QMainWindow):
             self.ui.text_binary.setText(binary_text_string)
             self.ui.key_binary.setText(self.concatenate_list_data(self.return_haslo_8bit()))
             self.ui.encrypted_message.setText(self.xor(self.ui.text_binary.text(),self.ui.key_binary.text()))
+
+            self.save()
+
+    def save(self):
+        file =  open('output.txt','w')
+        file.write("Key: \n" + self.ui.key_binary.text())
+        file.write("\n")
+        file.write("\nEncrypted message: \n" + self.ui.encrypted_message.text())
+        file.write("\n")
+        file.write("\nGenerations:\n")
+        for n in self.cellular.generations_list:
+            for item in n:
+                file.write("%s" % item)
+            file.write("\n")
+        file.write("\n")
+        file.write("\nRules:\n")
+        if type(self.cellular.rules_list) is list:
+            for m in self.cellular.rules_list:
+                    file.write("%s\n" % m)
 
     @pyqtSlot()
     def decrypt_clicked(self):
@@ -87,9 +132,9 @@ class Secret_key(QMainWindow):
 
         return lista_pomocnicza
 
-    def concatenate_list_data(self, list):
+    def concatenate_list_data(self, lista_do_polaczenia):
         result = ''
-        for element in list:
+        for element in lista_do_polaczenia:
             result += str(element)
         return result
 
@@ -100,12 +145,14 @@ class Secret_key(QMainWindow):
             lista.append(str(tmp[licznik]))
         return lista
 
-    def losowe_zera_i_jedynki(self,ilosc_losowych_zer_i_jedynek):
+    def losowe_zera_i_jedynki(self, ilosc_losowych_zer_i_jedynek):
         lista = []
-        while ilosc_losowych_zer_i_jedynek < len(self.ui.text_to_encrypt_box.text()):
-            #bez tego czasem klucz generowal sie krotszy niz tekst do zaszyfrowania,ale tym to naprawilem
-            #hasztag programowanie ekstremalne
-            ilosc_losowych_zer_i_jedynek = len(self.ui.text_to_encrypt_box.text()) +random.randrange(len(self.ui.text_to_encrypt_box.text()))
+        if type(self.rules) is list:
+            ilosc_losowych_zer_i_jedynek = len(self.rules)
+        else:
+            ilosc_losowych_zer_i_jedynek = len(self.ui.text_to_encrypt_box.text()) + random.randrange(10)
+            while ilosc_losowych_zer_i_jedynek  < len(self.ui.text_to_encrypt_box.text()):
+                ilosc_losowych_zer_i_jedynek = len(self.ui.text_to_encrypt_box.text()) + random.randrange(10)
 
         for licznik in range(ilosc_losowych_zer_i_jedynek): #to zwraca losowy stan poczatkowy jezeli user nie wpisal
             lista.append(str(random.randrange(2)))
